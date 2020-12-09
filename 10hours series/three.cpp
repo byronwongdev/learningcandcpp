7422c57905e06723f38f9cd374a397416511a79c
byronwongdev <byronwongdev@gmail.com>
2020-12-09 22:48:13 +0800

    progress update

---
+++ 10hours series/three.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+    int slices;
+    cout << "YOOO how many pieces of pizza you eat?: ";
+    cin >> slices;
+    cout << "You Have " << slices << " slices of pizza." << std::endl;
+}
