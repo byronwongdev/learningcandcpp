16c14966532033a612dbbb10f9f02aa73f4db870
byronwongdev <byronwongdev@gmail.com>
2020-12-11 00:19:18 +0800

    progress update

---
+++ 10hours series/nine.cpp
@@ -0,0 +1,16 @@
+/*
+escape sequence
+*/
+#include <iostream>
+
+using std::cout;
+
+int main() // main function
+{
+    cout << "Hello\tThere" << std::endl ;
+    cout << "Hello\bThere" << std::endl ;
+    cout << "Hello\nThere" << std::endl ;
+    cout << "Hello\vThere" << std::endl ;
+    cout << "Hello\aThere" << std::endl ;
+}
+
