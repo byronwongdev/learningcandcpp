dc8e59c1ca31ea3947d1029565e9625d2911ed69
byronwongdev <byronwongdev@gmail.com>
2020-12-10 23:48:44 +0800

    progress update

--- 10hours series/eight.cpp
+++ 10hours series/eight.cpp
@@ -8,7 +8,16 @@ using std::cout;
 
 int main() // main function
 {
-    char x = 'A';           //8 bit
-    cout << x << std::endl;
+    char x = 'B';           //8 bit
+    cout << (int)x << std::endl;
+    // base on asciitable
+    char y = 65;
+    cout << y << std::endl;
+    
+    char z = 127;       //largest posibility
+    cout << z << std::endl;
+
+    unsigned char v = 129;
+    cout << v << std::endl;
 }
 
