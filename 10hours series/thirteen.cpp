6596a3f584a318c890147944d7d9e216ae0eb47c
byronwongdev <byronwongdev@gmail.com>
2020-12-13 20:04:04 +0800

    progress update

---
+++ 10hours series/thirteen.cpp
@@ -0,0 +1,15 @@
+/*
+constant
+*/
+#include <iostream>
+using std::cout;
+
+
+
+int main() // main function
+{
+    const int setting = 5;
+
+    enum { y = 100 };
+}
+
--- 10hours series/twelve.cpp
+++ 10hours series/twelve.cpp
@@ -2,16 +2,21 @@
 type
 */
 #include <iostream>
+#include <float.h>
 
 using std::cout;
 
 int main() // main function
 {
-    float a;
+    float a = 10.0 / 3;
     double b = 77000; //7.7E4
     long double c;
 
     // 7.7 * 10000 = 77000
+    cout << a << std::endl;
+    cout << b << std::endl;
+    cout << c << std::endl;
+
 
 }
 
